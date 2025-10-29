cd07ce5ae38381bb8ed296c25e70b8b49ffe967a
Virginia-Gamba-bee <soledadgamba_1@hotmail.com>
2025-10-29 18:08:15 -0300

    cambio import
    Se agrego los import de java para conectar con la base de dato

--- proyectobd/src/main/java/confg/ConexcionBD.java
+++ proyectobd/src/main/java/confg/ConexcionBD.java
@@ -1,15 +1,16 @@
-package confg;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexcionBD {
 
     private static final String URL = "jdc:mysql://localhost:3307/grupo19";
 
-    private static final String USER = "root";
+    private static final String USER = "Virginia";
 
     private static final String PASSWORD = "Vir474";
 
-    public static Connection getConeConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
